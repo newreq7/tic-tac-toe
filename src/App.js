58d77759f2f4249b05bf07120ab2e4d7e71d8e8a
newreq7 <r2d2.benz@yandex.ru>
2023-02-14 12:38:19 +0300

    deleting unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [field, setField] = useState(null);
+
   return (
     <div id="root">
-      <button class="button_cleanup">cleanup</button>
+      <button class="button_cleanup" cleanup = {() => setField(null)}>cleanup</button>
       <div class="grid-container">
-          <div class="item item1"></div>
-          <div class="item item2"></div>
-          <div class="item item3"></div>
-          <div class="item item4"></div>
-          <div class="item item5"></div>
-          <div class="item item6"></div>
-          <div class="item item7"></div>
-          <div class="item item8"></div>
-          <div class="item item9"></div>
+          <div class="item item1">1</div>
+          <div class="item item2">2</div>
+          <div class="item item3">3</div>
+          <div class="item item4">4</div>
+          <div class="item item5">5</div>
+          <div class="item item6">6</div>
+          <div class="item item7">7</div>
+          <div class="item item8">8</div>
+          <div class="item item9">9</div>
       </div>
     </div>
   );
 }
 
 
-export default App;
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Вы кликнули {count} раз(а)</p>
+      <button onClick={() => setCount(count + 1)}>
+       Нажми на меня
+      </button>
+    </div>
+  );
+}
+
+
+  export default (App);
